97dcdb6e83b38ea49e6675eafe5b3e7169783f55
aspen <jgromadzinska9@gmail.com>
2022-07-10 19:51:10 +0100

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,25 @@
-function computerPlay () {
+function computerPlay() {
  var choice= ["Rock", "Paper", "Scissors"];
  var randomChoice = choice[Math.floor(Math.random() * choice.length)];
  return randomChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper") {
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    } else if (playerSelection == computerSelection) {
+        return `${playerSelection} and ${computerSelection}. It's a tie!`
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    }
+}
+
+function capitalize(string) {
+    let lowerCase = string.toLowerCase();
+    let upperCase = lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
+    return upperCase;
+}
+
+const playerSelection = capitalize(prompt("Rock, Paper, Scissors. Choose one."));
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
