121eb01877606421c483179606b865494c45e7d3
aspen <jgromadzinska9@gmail.com>
2022-09-11 16:38:14 +0100

    Change style and comments clean-up

--- javascript.js
+++ javascript.js
@@ -6,7 +6,6 @@ function computerPlay() {
 
 }
 
-//let computerSelection = computerPlay();
 
 //plays a single round of game
 function playRound() {
@@ -16,82 +15,31 @@ function playRound() {
         if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper") {
             roundsPlayed += 1;
             playerScore += 1;
-            //console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
             roundResult.innerHTML = `You win! ${playerSelection} beats ${computerSelection}!`;
             score.innerHTML = `Player score: ${playerScore}  Computer score: ${computerScore}`;
         } else if (playerSelection == computerSelection) {
-            //console.log(`${playerSelection} and ${computerSelection}. It's a tie!`);
             roundsPlayed += 1;
             roundResult.innerHTML = `${playerSelection} and ${computerSelection}. It's a tie!`;
             score.innerHTML = `Player score: ${playerScore}  Computer score: ${computerScore}`;
         } else {
             roundsPlayed += 1;
             computerScore += 1;
-            //console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
             roundResult.innerHTML = `You lose! ${computerSelection} beats ${playerSelection}.`;
             score.innerHTML = `Player score: ${playerScore}  Computer score: ${computerScore}`;
         }
     }
-    //if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper") {
-        //roundsPlayed += 1;
-       // playerScore += 1;
-        //console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
-       // roundResult.innerHTML = `You win! ${playerSelection} beats ${computerSelection}!`;
-       // score.innerHTML = `Player score: ${playerScore}  Computer score: ${computerScore}`;
-    //} else if (playerSelection == computerSelection) {
-        //console.log(`${playerSelection} and ${computerSelection}. It's a tie!`);
-        //roundsPlayed += 1;
-       // roundResult.innerHTML = `${playerSelection} and ${computerSelection}. It's a tie!`;
-        //score.innerHTML = `Player score: ${playerScore}  Computer score: ${computerScore}`;
-   // } else {
-        //roundsPlayed += 1;
-        //computerScore += 1;
-        //console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
-       // roundResult.innerHTML = `You lose! ${computerSelection} beats ${playerSelection}.`;
-       // score.innerHTML = `Player score: ${playerScore}  Computer score: ${computerScore}`;
-    //}
 }
 
 let playerSelection;
 
 let computerSelection;
 
-//turns the first letter of a string into uppercase and the rest into lowercase (e.g. 'ROCK', 'rock', 'rOcK' will all be turned into 'Rock')
-//function capitalize(string) {
-//let lowerCase = string.toLowerCase();
-//let upperCase = lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
-//return upperCase;
-//}
-
-//checks if playRound function works:
-//const playerSelection = capitalize(prompt("Rock, Paper, Scissors. Choose one."));
-//const computerSelection = computerPlay();
-//console.log(playRound(playerSelection, computerSelection));
-
 let playerScore = 0;
 let computerScore = 0;
 let roundsPlayed = 0;
 
-//plays five rounds of the game, records the score and announces the winner at the end
-//function game() {
-//for (let i = 0; i < 5; i++) {
-//const playerSelection = capitalize(prompt("Rock, Paper, Scissors. Choose one."));
-//const computerSelection = computerPlay();
-//playRound(playerSelection, computerSelection);
-//roundsPlayed += 1;
-//console.log(`Player: ${playerScore} Computer: ${computerScore}`);
-//if (roundsPlayed == 5 && playerScore > computerScore) {
-//console.log("You win this game!");
-//} else if (roundsPlayed == 5 && playerScore < computerScore) {
-//console.log("You lose this game!");
-//} else if (roundsPlayed == 5 && playerScore == computerScore) {
-//console.log("It's a tie!")
-//}
-//}
-//}
 
-//game();
-
+//announces the winner of the game
 function checkScore() {
     if ((playerScore == 5 || computerScore == 5) && playerScore > computerScore) {
         winOrLose.innerHTML = "Congratulations! You won the game.";
@@ -102,20 +50,13 @@ function checkScore() {
    }
 };
 
+//player selection
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
-    //alert("You chose rock");
     playerSelection = "Rock";
     computerSelection = computerPlay();
     playRound();
-    //roundsPlayed += 1;
     roundNumber.innerHTML = `Round: ${roundsPlayed}`;
-    //console.log(computerPlay());
-    //console.log(playerSelection);
-    //console.log(computerSelection);
-    //if (roundsPlayed == 5){
-    //    checkScore();
-    //}
     checkScore();
 });
 
@@ -124,7 +65,6 @@ paper.addEventListener('click', () => {
     playerSelection = "Paper";
     computerSelection = computerPlay();
     playRound();
-    //roundsPlayed += 1;
     roundNumber.innerHTML = `Round: ${roundsPlayed}`;
     checkScore();
 });
@@ -134,7 +74,6 @@ scissors.addEventListener('click', () => {
     playerSelection = "Scissors";
     computerSelection = computerPlay();
     playRound();
-    //roundsPlayed += 1;
     roundNumber.innerHTML = `Round: ${roundsPlayed}`;
     checkScore();
 });
